1afcfa143491af5e45b32f33b4043398e3bce037
iqionly <mochammadrizkyashyari@gmail.com>
2026-01-09 21:48:06 +0700

    CH7

--- Game1.cs
+++ Game1.cs
@@ -2,12 +2,16 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
+using System;
+using System.Diagnostics;
 
 namespace SnakeBounce
 {
     public class Game1 : Core
     {
-        private Texture2D _logo;
+        private TextureRegion _snake;
+        private TextureRegion _apple;
 
         public Game1() : base("SnakeBounce", 1280, 720, false)
         {
@@ -23,11 +27,11 @@ namespace SnakeBounce
 
         protected override void LoadContent()
         {
-            // TODO: use this.Content to load your game content here
+            // Create the texture atlas from the XML configuration file
+            TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
 
-            base.LoadContent();
-
-            _logo = Content.Load<Texture2D>("images/logo");
+            _snake = atlas.GetRegion("snake-head-right");
+            _apple = atlas.GetRegion("apple");
         }
 
         protected override void Update(GameTime gameTime)
@@ -44,11 +48,16 @@ namespace SnakeBounce
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            // Begin the sprite batch to prepare for rendering.
             SpriteBatch.Begin();
 
-            SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+            // Draw the slime texture region at a scale of 4.0
+            _snake.Draw(SpriteBatch, Vector2.Zero, Color.White, 0.0f, Vector2.One, 1.0f, SpriteEffects.None, 0.0f);
 
+            // Draw the bat texture region 10px to the right of the slime at a scale of 4.0
+            _apple.Draw(SpriteBatch, new Vector2(_snake.Width * 4.0f + 10, 0), Color.White, 0.0f, Vector2.One, 1.0f, SpriteEffects.None, 1.0f);
+
+            // Always end the sprite batch when finished.
             SpriteBatch.End();
 
             base.Draw(gameTime);

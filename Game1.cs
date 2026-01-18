c5cbd8d04d400d7099a088a926fa0ccfb4cee618
iqionly <mochammadrizkyashyari@gmail.com>
2026-01-18 19:07:56 +0700

    CH 12: Collision Detection + Apple Movements + Boundings Snake & Apple + Spawn Snake on center screen

--- Game1.cs
+++ Game1.cs
@@ -17,6 +17,11 @@ namespace SnakeBounce
         // Tracks the position of the slime.
         private Vector2 _snakePosition;
 
+        // Tracks the position of the apple.
+        private Vector2 _applePosition;
+
+        private Vector2 _appleVelocity;
+
         // Speed multiplier when moving.
         private const float MOVEMENT_SPEED = 5.0f;
 
@@ -33,10 +38,18 @@ namespace SnakeBounce
 
         protected override void Initialize()
         {
-            // In initialization code:
+            base.Initialize();
+
             _inputBuffer = new Queue<Vector2>(MAX_BUFFER_SIZE);
 
-            base.Initialize();
+            _snakePosition = new Vector2(
+                GraphicsDevice.PresentationParameters.BackBufferWidth * 0.5f,
+                GraphicsDevice.PresentationParameters.BackBufferHeight * 0.5f);
+
+            _applePosition = new Vector2(_snake.Width + 10, 0);
+
+            // Assign the initial random velocity to the bat.
+            AssignRandomBatVelocity();
         }
 
         protected override void LoadContent()
@@ -54,11 +67,114 @@ namespace SnakeBounce
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            // Update apple animations
+            _apple.Update(gameTime);
+
             // Update snake animations
             _snake.Update(gameTime);
 
-            // Update apple animations
-            _apple.Update(gameTime);
+            // Create a bounding rectangle for the screen.
+            Rectangle screenBounds = new Rectangle(
+                0,
+                0,
+                GraphicsDevice.PresentationParameters.BackBufferWidth,
+                GraphicsDevice.PresentationParameters.BackBufferHeight
+            );
+
+            // Creating a bounding circle for the slime
+            Circle slimeBounds = new Circle(
+                (int)(_snakePosition.X + (_snake.Width * 0.5f)),
+                (int)(_snakePosition.Y + (_snake.Height * 0.5f)),
+                (int)(_snake.Width * 0.5f)
+            );
+
+            // Use distance based checks to determine if the slime is within the
+            // bounds of the game screen, and if it is outside that screen edge,
+            // move it back inside.
+            if (slimeBounds.Left < screenBounds.Left)
+            {
+                _snakePosition.X = screenBounds.Left;
+            }
+            else if (slimeBounds.Right > screenBounds.Right)
+            {
+                _snakePosition.X = screenBounds.Right - _snake.Width;
+            }
+
+            if (slimeBounds.Top < screenBounds.Top)
+            {
+                _snakePosition.Y = screenBounds.Top;
+            }
+            else if (slimeBounds.Bottom > screenBounds.Bottom)
+            {
+                _snakePosition.Y = screenBounds.Bottom - _snake.Height;
+            }
+
+            // Calculate the new position of the bat based on the velocity.
+            Vector2 newBatPosition = _applePosition + _appleVelocity;
+
+            // Create a bounding circle for the bat.
+            Circle batBounds = new Circle(
+                (int)(newBatPosition.X + (_apple.Width * 0.5f)),
+                (int)(newBatPosition.Y + (_apple.Height * 0.5f)),
+                (int)(_apple.Width * 0.5f)
+            );
+
+            Vector2 normal = Vector2.Zero;
+
+            // Use distance based checks to determine if the bat is within the
+            // bounds of the game screen, and if it is outside that screen edge,
+            // reflect it about the screen edge normal.
+            if (batBounds.Left < screenBounds.Left)
+            {
+                normal.X = Vector2.UnitX.X;
+                newBatPosition.X = screenBounds.Left;
+            }
+            else if (batBounds.Right > screenBounds.Right)
+            {
+                normal.X = -Vector2.UnitX.X;
+                newBatPosition.X = screenBounds.Right - _apple.Width;
+            }
+
+            if (batBounds.Top < screenBounds.Top)
+            {
+                normal.Y = Vector2.UnitY.Y;
+                newBatPosition.Y = screenBounds.Top;
+            }
+            else if (batBounds.Bottom > screenBounds.Bottom)
+            {
+                normal.Y = -Vector2.UnitY.Y;
+                newBatPosition.Y = screenBounds.Bottom - _apple.Height;
+            }
+
+            // If the normal is anything but Vector2.Zero, this means the bat had
+            // moved outside the screen edge so we should reflect it about the
+            // normal.
+            if (normal != Vector2.Zero)
+            {
+                normal.Normalize();
+                _appleVelocity = Vector2.Reflect(_appleVelocity, normal);
+            }
+
+            _applePosition = newBatPosition;
+
+            if (slimeBounds.Intersects(batBounds))
+            {
+                // Divide the width  and height of the screen into equal columns and
+                // rows based on the width and height of the bat.
+                int totalColumns = GraphicsDevice.PresentationParameters.BackBufferWidth / (int)_apple.Width;
+                int totalRows = GraphicsDevice.PresentationParameters.BackBufferHeight / (int)_apple.Height;
+
+                // Choose a random row and column based on the total number of each
+                int column = Random.Shared.Next(0, totalColumns);
+                int row = Random.Shared.Next(0, totalRows);
+
+                // Change the bat position by setting the x and y values equal to
+                // the column and row multiplied by the width and height.
+                _applePosition = new Vector2(column * _apple.Width, row * _apple.Height);
+
+                // Assign a new random velocity to the bat
+                AssignRandomBatVelocity();
+            }
 
             CheckKeyboardInputBuffer();
 
@@ -186,18 +302,32 @@ namespace SnakeBounce
             _snakePosition += _lastDirection * speed;
         }
 
+        private void AssignRandomBatVelocity()
+        {
+            // Generate a random angle.
+            float angle = (float)(Random.Shared.NextDouble() * Math.PI * 2);
+
+            // Convert angle to a direction vector.
+            float x = (float)Math.Cos(angle);
+            float y = (float)Math.Sin(angle);
+            Vector2 direction = new Vector2(x, y);
+
+            // Multiply the direction vector by the movement speed.
+            _appleVelocity = direction * MOVEMENT_SPEED;
+        }
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // Begin the sprite batch to prepare for rendering.
-            SpriteBatch.Begin();
+            SpriteBatch.Begin(SpriteSortMode.BackToFront);
 
             // Draw the slime sprite.
             _snake.Draw(SpriteBatch, _snakePosition);
 
             // Draw the bat sprite 10px to the right of the slime.
-            _apple.Draw(SpriteBatch, new Vector2(_snake.Width + 10, 0));
+            _apple.Draw(SpriteBatch, _applePosition);
 
             // Always end the sprite batch when finished.
             SpriteBatch.End();
